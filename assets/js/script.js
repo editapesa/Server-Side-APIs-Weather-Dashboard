f49b5bcf07eeb6ac34329f886f2ed5e601aae39a
Edita P <blugirlnyc@hotmail.com>
2021-03-05 17:11:04 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var searchHistory = document.getElementById('city-search');
+var chosenCity = document.getElementById('city-name');
+var key = "c8991a84574210433ef4c391a616abb1";
+
+//function to enter city for search
+
+
+//function to get weather from api 
