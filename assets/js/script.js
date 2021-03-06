88b0b03e386ba3cb4d903251ffbee4d1a40d6f7f
Edita P <blugirlnyc@hotmail.com>
2021-03-05 22:51:55 -0500

    create empty container in html for 5 day forecast, add vars to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
-var searchHistory = document.getElementById('city-search');
-var chosenCity = document.getElementById('city-name');
+var searchButton = $('.searchBtn');
+var citySearch = S('.city-input');
+
 var key = "c8991a84574210433ef4c391a616abb1"; 
+
+var chosenCityEl = $('.chosen-city');
+var currentDateEl = $('current-date')
+ 
+var todaysDate = moment().format("MMM Do YYYY");
+
 
 //function to enter city for search
 
 
 //function to get weather from api 
+https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+//local storage to store city search history
+for (var i = 0; i < localStorage.length; i++) {
+     var city = localStorage.getItem(i);
+}
+
+// $('.saveBtn').on('click', function () {
+//     var eventDescription = $(this).siblings('.description').val();
+//     var timeOfDay = $(this).parent().attr('id');
+
+//     localStorage.setItem(timeOfDay, eventDescription);
+// });
+
